251b6d7cb8e0f480cf054166a3d81d378be983b4
Jose Evans <jpjoao24@hotmail.com>
2022-11-18 20:53:04 +0000

    1ª Entrega concluida

--- criar.sql
+++ criar.sql
@@ -15,9 +15,9 @@ CREATE TABLE Arbitro (
 
 DROP TABLE IF EXISTS Equipa;
 CREATE TABLE Equipa (
-    nome VARCHAR (150) PRIMARY KEY ON CONFLICT IGNORE, 
+    nome VARCHAR (150) PRIMARY KEY, 
     email VARCHAR (150) UNIQUE NOT NULL, 
-    telefone VARCHAR (150) UNIQUE, 
+    telefone VARCHAR (150) UNIQUE NOT NULL, 
     morada VARCHAR (150) NOT NULL UNIQUE, 
     pontuacaoTotal INTEGER DEFAULT (0), 
     golosMarcados INTEGER DEFAULT (0), 
@@ -53,23 +53,23 @@ CREATE TABLE Jogador (
 DROP TABLE IF EXISTS Jornada;
 CREATE TABLE Jornada (
     numero INTEGER PRIMARY KEY, 
-    dataInicio VARCHAR (75) NOT NULL, 
-    dataFim VARCHAR (75) NOT NULL
+    dataInicio VARCHAR (75) UNIQUE NOT NULL, 
+    dataFim VARCHAR (75) UNIQUE NOT NULL
 );
 
 DROP TABLE IF EXISTS Playoff;
 CREATE TABLE Playoff (
-    fase VARCHAR (75) PRIMARY KEY NOT NULL, 
-    dataInicio VARCHAR (75) NOT NULL, 
-    dataFim VARCHAR (75) NOT NULL
+    fase VARCHAR (75) PRIMARY KEY CHECK (fase = "QUARTOS-DE-FINAL" or fase = "MEIAS-FINAIS" or fase = "FINAL") NOT NULL, 
+    dataInicio VARCHAR (75) UNIQUE NOT NULL, 
+    dataFim VARCHAR (75) UNIQUE NOT NULL
 );
 
 DROP TABLE IF EXISTS Jogo;
 CREATE TABLE Jogo (
     idJogo INTEGER PRIMARY KEY, 
     data VARCHAR (75) NOT NULL, 
-    golosMarcadosVisitante INTEGER, 
-    golosMarcadosVisitada INTEGER, 
+    golosMarcadosVisitante INTEGER CHECK (golosMarcadosVisitante >= 0 ) NOT NULL, 
+    golosMarcadosVisitado INTEGER CHECK (golosMarcadosVisitado >= 0) NOT NULL, 
     nomeEquipaVisitada VARCHAR (75) REFERENCES Equipa (nome) ON DELETE CASCADE ON UPDATE CASCADE, 
     nomeEquipaVisitante VARCHAR (75) REFERENCES Equipa (nome) ON DELETE CASCADE ON UPDATE CASCADE, 
     arbitro INTEGER REFERENCES Pessoa (idPessoa) ON DELETE CASCADE ON UPDATE CASCADE, 

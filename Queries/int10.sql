e6f1363365cec11eecfd94e7e63db6f8fac07fbe
Adriano Machado <93844395+Adriano-7@users.noreply.github.com>
2022-12-11 21:18:38 +0000

    Started working on queries 6-10

---
+++ Queries/int10.sql
@@ -0,0 +1,12 @@
+/*jogador com mais golos marcados por equipa*/
+select nome, primeiroNome, segundoNome, max(golos) as golos
+from
+(
+select Equipa.nome, Pessoa.primeiroNome, Pessoa.segundoNome ,count(GoloMarcado.idGolo) as golos
+from GoloMarcado
+inner join Jogador on Jogador.idJogador = GoloMarcado.idJogador
+inner join Pessoa on Jogador.idJogador = Pessoa.idPessoa
+inner join Equipa on Jogador.equipa = Equipa.nome
+group by Jogador.idJogador
+)
+GROUP BY nome

d8439598df76150efe68845fd75c52ea50feb609
Adriano Machado <93844395+Adriano-7@users.noreply.github.com>
2022-12-21 16:21:02 +0000

    Solved issue, now states are updated for both teams

--- Gatilhos/gatilho1_adiciona.sql
+++ Gatilhos/gatilho1_adiciona.sql
@@ -2,12 +2,17 @@ PRAGMA foreign_keys = ON;
 
 CREATE TRIGGER gatilho1
 AFTER INSERT ON Jogo
+FOR EACH ROW
 BEGIN
-   UPDATE Equipa
-    SET pontuacaoTotal = pontuacaoTotal + (SELECT CASE WHEN NEW.golosMarcadosVisitante > NEW.golosMarcadosVisitado THEN 3 WHEN NEW.golosMarcadosVisitante < NEW.golosMarcadosVisitado THEN 0 ELSE 1 END),
-    golosMarcados = golosMarcados + NEW.golosMarcadosVisitante,
-    golosSofridos = golosSofridos + NEW.golosMarcadosVisitado,
-    jogosGanhos = jogosGanhos + (SELECT CASE WHEN NEW.golosMarcadosVisitante > NEW.golosMarcadosVisitado THEN 1 ELSE 0 END),
-    jogosEmpatados = jogosEmpatados + (SELECT CASE WHEN NEW.golosMarcadosVisitante = NEW.golosMarcadosVisitado THEN 1 ELSE 0 END)
-    WHERE nome = NEW.nomeEquipaVisitante;
+    UPDATE Equipa
+    SET pontuacaoTotal = pontuacaoTotal + (CASE WHEN nome = NEW.nomeEquipaVisitante AND NEW.golosMarcadosVisitante > NEW.golosMarcadosVisitado THEN 3
+                                                WHEN nome = NEW.nomeEquipaVisitada AND NEW.golosMarcadosVisitado > NEW.golosMarcadosVisitante THEN 3
+                                                ELSE (CASE WHEN NEW.golosMarcadosVisitante = NEW.golosMarcadosVisitado THEN 1 ELSE 0 END) END),
+
+        golosMarcados = golosMarcados + (CASE WHEN nome = NEW.nomeEquipaVisitante THEN NEW.golosMarcadosVisitante ELSE (CASE WHEN nome = NEW.nomeEquipaVisitada THEN NEW.golosMarcadosVisitado ELSE 0 END) END),
+        golosSofridos = golosSofridos + (CASE WHEN nome = NEW.nomeEquipaVisitante THEN NEW.golosMarcadosVisitado ELSE (CASE WHEN nome = NEW.nomeEquipaVisitada THEN NEW.golosMarcadosVisitante ELSE 0 END) END),
+        jogosGanhos = jogosGanhos + (CASE WHEN nome = NEW.nomeEquipaVisitante AND NEW.golosMarcadosVisitante > NEW.golosMarcadosVisitado THEN 1
+                                         WHEN nome = NEW.nomeEquipaVisitada AND NEW.golosMarcadosVisitado > NEW.golosMarcadosVisitante THEN 1 ELSE 0 END),
+        jogosEmpatados = jogosEmpatados + (CASE WHEN NEW.golosMarcadosVisitante = NEW.golosMarcadosVisitado THEN 1 ELSE 0 END)
+    WHERE nome IN (NEW.nomeEquipaVisitante, NEW.nomeEquipaVisitada);
 END;
